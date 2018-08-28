0415f8fd995c4bdfd9b765457a88b82b7eef3871
Stepan <kurakste@gmail.com>
2018-08-28 19:47:30 +0300

    Some improvement in list-of-file.php view.

--- views/files/list-of-file.php
+++ views/files/list-of-file.php
@@ -13,31 +13,30 @@ $this->params['breadcrumbs'][] = $this->title;
         <a href="/files/upload">Загрузить</a>
     </h3>
     <ul>
-    <?php $formId =0; ?> 
-    <?php foreach ($files as $file) : ?>
-    <?php $formId++ ?>
+        <?php $formId =0; ?> 
+        <?php foreach ($files as $file) : ?>
+            <?php $formId++ ?>
+            <li>
+                <!-- <?= $file ?>  -->
+                <form action="files/rename" method="post" id ='save_<?= $formId ?>'>
+                    <input type="hidden" name="oldname" id="" value="<?= $file ?>" />
+                    <input type="tetx" name="newname" id="" value="<?= $file ?>" />
+                </form>
+                <form action="files/delete" method="post" id ='delete_<?= $formId ?>'>
+                    <input type="hidden" name="filename" id="filename" value="<?= $file ?>" />
+                </form>
+                <button class = "file-delete-button" 
+                        type ="submit" 
+                        form ="delete_<?= $formId ?>" >
+                        <i class="fas fa-trash-alt" aria-hidden="true"></i>
+                </button>
+                <button class = "file-save-button" 
+                        type ="submit" 
+                        form ="save_<?= $formId ?>" >
+                        <i class="fas fa-save" aria-hidden="true"></i>
 
-    <li>
-        <!-- <?= $file ?>  -->
-        <form action="files/rename" method="post" id ='save_<?= $formId ?>'>
-            <input type="hidden" name="oldname" id="" value="<?= $file ?>" />
-            <input type="tetx" name="newname" id="" value="<?= $file ?>" />
-        </form>
-        <form action="files/delete" method="post" id ='delete_<?= $formId ?>'>
-            <input type="hidden" name="filename" id="filename" value="<?= $file ?>" />
-        </form>
-        <button class = "file-delete-button" 
-                type ="submit" 
-                form ="delete_<?= $formId ?>" >
-                <i class="fas fa-trash-alt" aria-hidden="true"></i>
-        </button>
-        <button class = "file-save-button" 
-                type ="submit" 
-                form ="save_<?= $formId ?>" >
-                <i class="fas fa-save" aria-hidden="true"></i>
-
-                        </button> </li>
-    <?php endforeach ?>
+                </button> 
+            </li>
+        <?php endforeach ?>
     </ul>
-        
 </div>
