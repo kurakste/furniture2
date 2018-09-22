58fc5481ff3823d442e70fed1cad3015881ff677
Andryuhaha <intaberny@yandex.ru>
2018-09-22 21:34:27 +0300

    news page done

--- views/news/tape.php
+++ views/news/tape.php
@@ -1,21 +1,37 @@
-<div class="wrapper">
-    <div class="wrapper_target">
-        <h1><b>Ключевые новости отрасли</b></h1><hr>
+<?php
 
-        <?php foreach ($news as $new): ?>
-        <h1>
-            <?= $new->title ?>
-        </h1>
-        <h4>
-            <?= $new->created_at ?>
-        </h4>
-        <div class="wrapper_text">
-            <?= substr($new->content, 0, 500) ?>
+/* @var $this yii\web\View */
+
+$this->title = 'My Yii Application'; ?>
+
+
+
+    <div class="products-catagories-area clearfix">
+        <div class="amado-pro-catagory clearfix">
+            <div class="single-products-catagory clearfix" style="width: 85%;">
+
+                <div class="centered_rule">
+                    <h1><b>Ключевые новости отрасли</b></h1>
+                    <hr>
+
+                    <?php foreach ($news as $new): ?>
+                    <h1>
+                        <?= $new->title ?>
+                    </h1>
+                    <h4>
+                        <?= $new->created_at ?>
+                    </h4>
+                    <div class="wrapper_text">
+                        <?= substr($new->content, 0, 500) ?>
+                    </div>
+
+                    <a href="/news/read?id=<?= $new->id?>">...читать статью</a>
+                    <hr>
+                    <?php endforeach ?>
+
+                </div>
+            </div>
         </div>
-
-        <a href="/news/read?id=<?= $new->id?>">...читать статью</a>
-        <hr>
-        <?php endforeach ?>
+        <!-- amado-pro-catagory clearfix -->
     </div>
-
-</div>
+    <!-- products-catagories-area clearfix -->
