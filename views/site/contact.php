5b20379cf94940ecf0cf8a5059937ab9d032dbf7
Andryuhaha <intaberny@yandex.ru>
2018-09-22 22:31:29 +0300

    contact cards review

--- views/site/contact.php
+++ views/site/contact.php
@@ -1,20 +1,54 @@
-        <div class="cart-table-area section-padding-100">
-            <div class="container-fluid">
-                <div class="row">
-                    <div class="col-12 col-lg-8">
-                        <div class="checkout_details_area mt-50 clearfix">
+<div class="cart-table-area section-padding-100">
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-12 col-lg-8">
+                <div class="checkout_details_area mt-50 clearfix">
 
-                            <div class="cart-title">
-                                <h2>Контакты</h2>
-                                <h3>Телефон:</h3><a href="tel:+79123619431"><b>Позвонить опреатору</b></a>
-                                <h3>Электронная почта:</h3>
-                                <a href="mailto: chepetskscent@mail.ru?subject=Вопрос с сайта Чепецк Мебель"><b>chepetskscent@mail.ru</b></a>
-                                <script type="text/javascript" charset="utf-8" async src="https://api-maps.yandex.ru/services/constructor/1.0/js/?um=constructor%3Ada9d9a88641469456b35daf0323b196f989afcb71e2fda5c85ca8e2e2892f55a&amp;width=701&amp;height=546&amp;lang=ru_RU&amp;scroll=true"></script>
-                            </div>
-                        </div>
+                    <div class="cart-title">
+                        <h2>Контакты</h2>
+                        <hr>
+                        <h3>Остались вопросы? </h3><a href="tel:+79123619431"><b><h4>&#9742; Позвонить оператору, жми на ссылку &#9742;</h4></b></a><br>
+                        <h3>Вы можете написать нам:</h3>
+                        <a href="mailto: chepetskscent@mail.ru?subject=Вопрос с сайта Чепецк Мебель"><b><h4>&#9998;Написать оператору, жми на ссылку&#9998;</h4></b></a> <br>
+
+                        <p>Индивидуальный предприниматель Арасланов Амир Мударисович<br> Реквизиты: </p>
+
+                        <p>Фактический адрес: </p>
+
+                        <p>613044, Кировская область, г. Кирово-Чепецк, ул. 60 лет Октября, 3/2 - 22, </p>
+
+                        <p> тел/факс (912) 361-94-31 </p>
+
+                        <p>ОГРНИП 316435000080367 , ИНН 431202300601 </p>
+
+                        <p>Юридический адрес: </p>
+
+                        <p> 613044, Кировская область, г. Кирово-Чепецк, ул. 60 лет Октября, 3/2 - 22, </p>
+
+                        <p>тел/факс (912) 361-94-31, (83361) 6-50-90 <br>
+                        </p>
+
+                        <p> <br> ИП Арасланов Амир Мударисович </p>
+
+                        <p>ИНН431202300601,ОГРНИП316435000080367 </p>
+
+                        <p>Банковские реквизиты: </p>
+
+                        <p>р/счет 40802810527000005814 в Кировском </p>
+
+                        <p>отделении №. 5612 ПАО «Сбербанк» г. Киров, БИК 043304609, </p>
+
+                        <p> кор/счет 30101810500000000609 </p>
+
+                        <p> <br>
+                        </p>
+
+                        <!--<script type="text/javascript" charset="utf-8" async src="https://api-maps.yandex.ru/services/constructor/1.0/js/?um=constructor%3Ada9d9a88641469456b35daf0323b196f989afcb71e2fda5c85ca8e2e2892f55a&amp;width=701&amp;height=546&amp;lang=ru_RU&amp;scroll=true"></script>-->
                     </div>
-                    
                 </div>
             </div>
+
         </div>
     </div>
+</div>
+</div>
