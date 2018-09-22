08c7b335e1e1c68d7b90b45625bd516780240fc2
Andryuhaha <intaberny@yandex.ru>
2018-09-22 16:33:07 +0300

    news

--- views/news/tape.php
+++ views/news/tape.php
@@ -1,13 +1,20 @@
 <div class="wrapper">
+    <div class="wrapper_target">
+        <h1><b>Ключевые новости отрасли</b></h1>
 
-    <?php foreach ($news as $new): ?>
-        <h1> <?= $new->title ?></h1>
-        <h4> <?= $new->created_at ?> </h4>
-        <div style = "width: 800px;">
+        <?php foreach ($news as $new): ?>
+        <h1>
+            <?= $new->title ?>
+        </h1>
+        <h4>
+            <?= $new->created_at ?>
+        </h4>
+        <div style="width: 800px;">
             <?= substr($new->content, 0, 500) ?>
         </div>
 
         <a href="/news/read?id=<?= $new->id?>">читать дальше</a>
-    <?php endforeach ?>
+        <?php endforeach ?>
+    </div>
 
 </div>
