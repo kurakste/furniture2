0a3347a83752307e19b22fcea2ccf91efdb716ba
kurakste <kurakste@gmail.com>
2018-10-02 15:08:43 -0400

    add counters

--- views/layouts/furniture.php
+++ views/layouts/furniture.php
@@ -13,6 +13,7 @@ $this->beginPage(); ?>
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="Магазин мебели от производителя. Стулья. Столы. Чепетская Мебельная фабрика." > 
     <title>Чепецкая мебельная фабрика</title>
     <link rel="icon" href="/icon/logo.png">
     <link href="https://fonts.googleapis.com/css?family=Montserrat+Alternates" rel="stylesheet">
@@ -145,6 +146,43 @@ $this->beginPage(); ?>
                 </div>
             </div>
         </footer>
+        <!-- Global site tag (gtag.js) - Google Analytics -->
+        <script async src="https://www.googletagmanager.com/gtag/js?id=UA-126282571-1"></script>
+        <script>
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+
+          gtag('config', 'UA-126282571-1');
+        </script>
+        <!-- Yandex.Metrika counter -->
+        <script type="text/javascript" >
+            (function (d, w, c) {
+                    (w[c] = w[c] || []).push(function() {
+                                try {
+                                                w.yaCounter50461330 = new Ya.Metrika2({
+                                                    id:50461330,
+                                                    clickmap:true,
+                                                    trackLinks:true,
+                                                    accurateTrackBounce:true
+                                                });
+                                            } catch(e) { }
+                            });
+            
+                    var n = d.getElementsByTagName("script")[0],
+                                s = d.createElement("script"),
+                    f = function () { n.parentNode.insertBefore(s, n); };
+                        s.type = "text/javascript";
+                            s.async = true;
+                            s.src = "https://mc.yandex.ru/metrika/tag.js";
+                            
+                                    if (w.opera == "[object Opera]") {
+                                                d.addEventListener("DOMContentLoaded", f, false);
+                                                        } else { f(); }
+                                })(document, window, "yandex_metrika_callbacks2");
+        </script>
+        <noscript><div><img src="https://mc.yandex.ru/watch/50461330" style="position:absolute; left:-9999px;" alt="" /></div></noscript>
+        <!-- /Yandex.Metrika counter -->
 
         <?php $this->endBody() ?>
     </body>
