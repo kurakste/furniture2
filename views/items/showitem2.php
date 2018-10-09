a41f615e8acf303c79a3bd53203cabbe14c4117f
Stepan <kurakste@gmail.com>
2018-10-09 23:53:52 +0300

    Change number format in markup

--- views/items/showitem2.php
+++ views/items/showitem2.php
@@ -50,7 +50,7 @@ use app\objects\widgets\cfselector\Colorandfactureselector;
                               <div itemprop="offers" itemscope itemtype="http://schema.org/Offer">
                                 <meta itemprop="price" content="<?= number_format($item->price, 2, '.', ' ')?>">
                                 <meta itemprop="priceCurrency" content="RUB">
-                                <p class="product-price"><?= number_format($item->price, 2, '.', ' ')?></p>
+                                <p class="product-price"><?= number_format($item->price, 0, '.', ' ')?></p>
                               </div> <!-- offers -->
                             <a href="#">
                                 <div itemprop="name">
